ff70a99223f14bf6858934b5d12ddd7eb0f9b451
jjd330 <jjd330@psu.edu>
2019-11-22 12:12:45 -0500

    Made it possible to pass a list of hosts and certificates

--- client/amon_client_post_ssl.py
+++ client/amon_client_post_ssl.py
@@ -1,12 +1,12 @@
 """@package amon_client_post_ssl
-client that sends events to the server using HTTP 
+client that sends events to the server using HTTP
 protocol with method=POST over TLS with certificate and key file client.crt and client.key
 """
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import sys, getopt, os, shutil, logging, datetime
+import sys, getopt, os, shutil, logging, datetime, time
 import resource
 import fcntl
 import traceback, io
@@ -14,7 +14,7 @@ import traceback, io
 from twisted.internet import reactor, ssl, tcp, interfaces
 from twisted.python import usage, log
 from twisted.internet.task import LoopingCall
-from twisted.internet.defer import Deferred, succeed
+from twisted.internet.defer import Deferred, succeed, DeferredList
 from twisted.internet.protocol import Protocol
 from twisted.web.client import Agent, FileBodyProducer
 from twisted.web import http_headers
@@ -43,27 +43,43 @@ class ResourcePrinter(Protocol):
 def printResource(response):
     finished = Deferred()
     response.deliverBody(ResourcePrinter(finished))
-    MOVEFILE=True            
     return finished
 
 def printError(failure):
-    MOVEFILE=False
     failure.value.reasons[0].printTraceback()
+    return failure
+
+
+def moveFile_OnSucess(result, oldpos, newpos):
+
+    # time.sleep(2.0)
+    MOVE = True
+    for (success, value) in result:
+        if success:
+            print('Success:', value)
+        else:
+            print('Failure:', value.getErrorMessage())
+            MOVE = False
+    if MOVE:
+        shutil.move(oldpos,newpos)
+        print("old location: %s, new location %s"%(oldpos, newpos))
+    return result
+
 
 
 def stop(result):
     reactor.stop()
-    
+
 def moveFile(path, fname):
-    shutil.move(path+fname, path+"archive/"+fname) 
-    print("File %s sent" % (fname,)) 
-    
+    shutil.move(path+fname, path+"archive/"+fname)
+    print("File %s sent" % (fname,))
+
 def printSent(fname):
     print("File %s sent" % (fname,))
-     
+
 def printNotSent(filename):
-    print("File %s not sent" % (fname,)) 
-    
+    print("File %s not sent" % (fname,))
+
 def check_open_fds():
     fds = []
     soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
@@ -74,7 +90,7 @@ def check_open_fds():
             continue
         fds.append(fd)
     return soft, fds
-    
+
 
 class MyClientContextFactory(object):
     """A context factory for SSL clients."""
@@ -91,15 +107,16 @@ class MyClientContextFactory(object):
         ctx.use_certificate_file(self.certificateFileName)
         ctx.use_privatekey_file(self.privateKeyFileName)
         return ctx
-                
+
 def check_for_files(hostport, eventpath, finaldir, keyfile, certfile):
     # check a directory with events (eventpath) for an oldest xml file
     # if found post it to the server (hostport) using HTTP POST protocol
-    
+
     # check for number of open file descriptors first, OS X is limited by default to 256
-    # each OS has its own limitation, file descriptors are open not only when the file 
+    # each OS has its own limitation, file descriptors are open not only when the file
     # is open, but for each new socket/connection opened as well
-    
+
+    # log.msg("Entering check_for_files", logLevel=logging.DEBUG)
     fds = []
     soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
     for fd in range(0, soft):
@@ -108,19 +125,23 @@ def check_for_files(hostport, eventpath, finaldir, keyfile, certfile):
         except IOError:
             continue
         fds.append(fd)
-        
+
     if (len(fds)<=soft):
-        host=hostport.encode()
+        Nhosts = len(hostport)
+        hosts = hostport
+        fkeys = keyfile
+        fcerts = certfile
+        # host=hostport.encode()
         path=eventpath
         fdir = finaldir
-        fkey = keyfile
-        fcert = certfile
-                
-        agent = Agent(reactor, MyClientContextFactory(fcert, fkey))
-        
+        # fkey = keyfile
+        # fcert = certfile
+
+        # agent = Agent(reactor, MyClientContextFactory(fcert, fkey))
+
         files = sorted(os.listdir(path), key=lambda p: os.path.getctime(os.path.join(path, p)))
         files_xml=[]
-      
+
         for filename in files:
             if (os.path.isdir(path+filename) or filename[0]=='.' or filename.find(".log") !=-1):
                 pass
@@ -128,41 +149,53 @@ def check_for_files(hostport, eventpath, finaldir, keyfile, certfile):
                 files_xml.append(filename)
             else:
                 pass
-            
+
+        # log.msg("%d xml files in directory"%(len(files_xml)), logLevel=logging.DEBUG)
         if len(files_xml)>0:
-            oldest = files_xml[0] 
+            oldest = files_xml[0]
             oldpos = os.path.join(path, oldest)
             newpos = os.path.join(path, fdir, oldest)
-            
-            try:
-                datafile=open(oldpos,'rb')
-                body=FileBodyProducer(datafile)
-                # since twisted v.15 length is string
-                length_data=str(body.length)
-                headers = http_headers.Headers({'User-Agent': ['Twisted HTTP Client'],
-                                            'Content-Type':['text/xml'], 
-                                            'Content-Lenght': [length_data],
-                                            'Content-Name':[oldest]})
-                method = b'POST'#.encode()
-                d = agent.request(method, host, headers=headers, bodyProducer=body)
-                # on success it returns Deferred with a response object
-                print("\nFound file")
-                d.addCallbacks(printResource, printError)
-                if MOVEFILE:
-                    shutil.move(oldpos,newpos)
-                    print("old location: %s, new location %s"%(oldpos, newpos))
-                else:
-                    print("Check connection. File not moved")
-            except (RuntimeError, TypeError, NameError, AttributeError, Exception) as e:
-                log.msg("Error parsing file %s " % (oldest,))
-                log.msg("Error: "+str(e))
-                log.msg(traceback.print_exc())
-            else:
-                print("ping db")
-            
+
+            d_list = []
+
+            for host,fkey,fcert in zip(hosts,fkeys,fcerts):
+                try:
+                    print(host,fkey,fcert)
+                    agent = Agent(reactor, MyClientContextFactory(fcert, fkey))
+
+                    datafile=open(oldpos,'rb')
+                    body=FileBodyProducer(datafile)
+                    # since twisted v.15 length is string
+                    length_data=str(body.length)
+                    headers = http_headers.Headers({'User-Agent': ['Twisted HTTP Client'],
+                                                'Content-Type':['text/xml'],
+                                                'Content-Lenght': [length_data],
+                                                'Content-Name':[oldest]})
+                    method = b'POST'#.encode()
+                    d = agent.request(method, host, headers=headers, bodyProducer=body)
+                    # on success it returns Deferred with a response object
+                    print("\nFound file")
+                    d.addCallbacks(printResource, printError)
+                    d_list.append(d)
+                except (RuntimeError, TypeError, NameError, AttributeError, Exception) as e:
+                    log.msg("Error sending file %s to host %s" % (oldest,host))
+                    log.msg("Error: "+str(e))
+                    log.msg(traceback.print_exc())
+                    global MOVEFILE
+                    MOVEFILE=False
+
+            dl = DeferredList(d_list, consumeErrors=True)
+            dl.addCallback(moveFile_OnSucess,oldpos,newpos)
+            # time.sleep(3.0)
+            # log.msg("Exiting check_for_files", logLevel=logging.DEBUG)
+            return dl
+
+            # if MOVEFILE:
+            #     shutil.move(oldpos,newpos)
+            #     print("old location: %s, new location %s"%(oldpos, newpos))
+            # else:
+            #     print("Check connection. File not moved")
         else:
             pass
     else:
-        pass          
-    
-
+        pass
--- twisted/plugins/amon_client_post_ssl_plugin.py
+++ twisted/plugins/amon_client_post_ssl_plugin.py
@@ -1,8 +1,8 @@
 """@package amon_client_post_events_ssl
-client that sends events to the server using HTTP 
+client that sends events to the server using HTTP
 protocol with method=POST
 Run with twistd. Default is run as a daemon process.
- twistd -l client.log --pidfile client.pid clientpostssl --hostport "<amon-hostport>" 
+ twistd -l client.log --pidfile client.pid clientpostssl --hostport "<amon-hostport>"
  --epath /path_to_client_ents --cfile client.crt --kfile client.key
  Kill it with kill `cat client.pid`
  Before running make directory archive within directory where your client events live.
@@ -16,39 +16,70 @@ import fcntl
 from zope.interface import implements,implementer
 from twisted.application import internet, service
 from twisted.application.internet import TimerService
+from twisted.application.internet import task
 from twisted.plugin import IPlugin
 from twisted.python import usage, log
 
 from client.amon_client_post_ssl import check_for_files
 from twisted.python import usage, log
-# Make a plugin using IServiceMaker                
- 
+# Make a plugin using IServiceMaker
+
+
+
 class Options(usage.Options):
 
     optParameters = [
-        ['hostport', 'hp', None, 'The host for https address.'],
+        ['hostport', 'hp', None, 'The host for https address. For multiple hosts supply them seperated by commas. '],
         ['epath', None, None, 'Path to the directory with VOEvents'],
-        ['finaldir', None, None, 'Directory name for final position'],
-        ['kfile', None, None, 'Key file name'],
-        ['cfile', None, None, 'Certificate name'],
+        ['finaldir', None, 'archive', 'Directory name for final position inside epath'],
+        ['kfile', None, None, 'Key file name. For multiple key files supply them seperated by commas. '],
+        ['cfile', None, None, 'Certificate name. For multiple hosts supply cert file names seperated by commas. '],
         ['looptime','lt',10.0,'How often to check epath for files in seconds'],
         ]
-#@implementer(service.IServiceMaker, IPlugin)	
-@implementer(IPlugin, service.IServiceMaker)	
+
+    def postOptions(self):
+
+        Nhosts = len(self['hostport'].split(','))
+        hosts = [host.strip().encode() for host in self['hostport'].split(',')]
+        fkeys = [fkey.strip() for fkey in self['kfile'].split(',')]
+        fcerts = [fcert.strip() for fcert in self['cfile'].split(',')]
+        if Nhosts != len(fcerts):
+            raise usage.UsageError("There must be one certificate file for each host")
+        elif Nhosts != len(fkeys) and len(fkeys) > 1:
+            raise usage.UsageError("There must be one key file for each host or only one key file to use for all hosts")
+
+        for fkey in fkeys:
+            if not os.path.exists(fkey):
+                raise usage.UsageError("keyfile %s doesn't exist"%(fkey))
+        for fcert in fcerts:
+            if not os.path.exists(fcert):
+                raise usage.UsageError("certfile %s doesn't exist"%(fkey))
+
+        self['hostport'] = hosts
+        if len(fkeys) < Nhosts:
+            self['kfile'] = [fkeys[0] for i in range(Nhosts)]
+        else:
+            self['kfile'] = fkeys
+        self['cfile'] = fcerts
+
+
+
+#@implementer(service.IServiceMaker, IPlugin)
+@implementer(IPlugin, service.IServiceMaker)
 class ClientPostServiceMaker(object):
-    
+
     #implements(service.IServiceMaker, IPlugin)
-         
+
     tapname = "clientpostssl"
     description = "Client http post events + ssl."
     options = Options
-    
+
     def makeService(self, options):
         #check directory with events for an oldest file
-        loop_service = TimerService(float(options['looptime']), check_for_files, options['hostport'], options['epath'], options['finaldir'],
-                        options['kfile'], options['cfile'])
-        loop_service.startService()
+        loop_service = TimerService(float(options['looptime']), check_for_files, options['hostport'],\
+                                    options['epath'], options['finaldir'], options['kfile'], options['cfile'])
+        # loop_service.startService()
         return loop_service
-        
-# finally, make a plugin 
-service_maker = ClientPostServiceMaker() 
+
+# finally, make a plugin
+service_maker = ClientPostServiceMaker()
